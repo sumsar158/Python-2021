ce861c0443a889aefd17c95bf5197a155ef3c98c
rapidi <rapidi@ttu.ee>
2021-08-30 18:53:35 +0300

    cahsier commit 77777875453

--- EX/ex01_hello/hello.py
+++ EX/ex01_hello/hello.py
@@ -1,42 +1,42 @@
-# """EX01 hello."""
-#
-# """
-# 1. Print Hello
-# Example output:
-#
-# What is your name? Mari
-# Hello, Mari! Enter a random number: 5
-# Great! Now enter a second random number: 4
-# 5 + 4 is 9
-#
-# """
-# name = input("What  is your name? ")
-# num1 = int(input(f"Hello, {name}! Enter a random number: "))
-# num2 = int(input("Great! Now enter a second random number: "))
-#
-# answer = num1 + num2
-#
-# print(f"{num1} + {num2} is {answer}")
-#
-# """
-# 2. Poem
-# Example output:
-#
-# Roses are red,
-# violets are blue,
-# I love to code
-# And so will you!
-#
-# """
-#
-# color = str(input("Roses are ..."))
-# objects = str(input("... are blue"))
-# activity = str(input("I love to ..."))
-#
-# print(f"Roses are {color}, \n"
-#       f"{objects} are blue, \n"
-#       f"I love to {activity} \n"
-#       f"And so will you!")
+"""EX01 hello."""
+
+"""
+1. Print Hello
+Example output:
+
+What is your name? Mari
+Hello, Mari! Enter a random number: 5
+Great! Now enter a second random number: 4
+5 + 4 is 9
+
+"""
+name = input("What  is your name? ")
+num1 = int(input(f"Hello, {name}! Enter a random number: "))
+num2 = int(input("Great! Now enter a second random number: "))
+
+answer = num1 + num2
+
+print(f"{num1} + {num2} is {answer}")
+
+"""
+2. Poem
+Example output:
+
+Roses are red,
+violets are blue,
+I love to code
+And so will you!
+
+"""
+
+color = str(input("Roses are ..."))
+objects = str(input("... are blue"))
+activity = str(input("I love to ..."))
+
+print(f"Roses are {color}, \n"
+      f"{objects} are blue, \n"
+      f"I love to {activity} \n"
+      f"And so will you!")
 
 """
 3. GreetingsGreetingsGreetings
