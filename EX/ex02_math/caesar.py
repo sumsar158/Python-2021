858c86a734313e7861a9cdd5bd260cbd09cbd549
rapidi <rapidi@ttu.ee>
2021-09-18 15:00:08 +0300

    4/4

---
+++ EX/ex02_math/caesar.py
@@ -0,0 +1,54 @@
+"""Caesar cipher."""
+import string
+
+
+def encode(message: str, shift: int) -> str:
+    """
+    Encode a message using a Caesar cipher.
+
+    Presume the message is already lowercase.
+    For each letter of the message, shift it forward in the alphabet by shift amount.
+    If the character isn't a letter, keep it the same.
+
+    For example, shift = 3 then a => d, b => e, z => c (see explanation below)
+
+    Shift:    0 1 2 3
+    Alphabet:       A B C D E F G H I J
+    Result:   A B C D E F G H I J
+
+    Examples:
+    1. encode('i like turtles', 6) == 'o roqk zaxzrky'
+    2. encode('example', 1) == 'fybnqmf'
+    3. encode('the quick brown fox jumps over the lazy dog.', 7) == 'aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.'
+
+    :param message: message to be encoded
+    :param shift: shift for encoding
+    :return: encoded message
+    """
+    alphabet = list(string.ascii_lowercase)
+    encrypted_message = []
+
+    for letter in message:
+        if letter in alphabet:
+            """finding the letters index in the alphabet."""
+            index_of_letter = alphabet.index(letter)
+            """increasing the letters index by 'shift' amount."""
+            shifted_index = (index_of_letter + shift) % 26
+            """finding the letter of shifted index."""
+            encrypted_letter = alphabet[shifted_index]
+            """adding shifted letter to new_message list."""
+            encrypted_message.append(encrypted_letter)
+
+        else:
+            encrypted_message.append(letter)
+
+    """ using ("".join(list_name) to turn list of encrypted letters into one string."""
+    return "".join(encrypted_message)
+
+
+if __name__ == '__main__':
+    print(encode("i like turtles", 6))  # -> o roqk zaxzrky
+    print(encode("o roqk zaxzrky", 20))  # -> i like turtles
+    print(encode("example", 1))  # -> fybnqmf
+    print(encode("don't change", 0))  # -> don't change
+    print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.
