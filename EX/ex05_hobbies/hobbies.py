4915aa33c942e99c9abc520ea828f6523851995c
rapidi <rapidi@ttu.ee>
2021-09-30 18:44:27 +0300

    test

--- EX/ex05_hobbies/hobbies.py
+++ EX/ex05_hobbies/hobbies.py
@@ -16,7 +16,13 @@ def sort_dictionary(dic: dict) -> dict:
     :param dic: dictionary to sort
     :return: sorted dictionary
     """
-    pass
+    sorted_dict = {}
+
+    for k, v in dic.items():
+        v = sorted(v)
+        sorted_dict[k] = v
+
+    return sorted_dict
 
 
 def create_dictionary(data: str) -> dict:
@@ -38,7 +44,6 @@ def create_dictionary(data: str) -> dict:
         if i[1] not in dict_hobbies[i[0]]:
             dict_hobbies[i[0]].append(i[1])
 
-    print(dict_hobbies)
     return dict_hobbies
 
 
@@ -49,7 +54,19 @@ def create_dictionary_with_hobbies(data: str) -> dict:
     :param data: given string from database
     :return: dictionary, where keys are hobbies and values are lists of people. Values are sorted alphabetically
     """
-    pass
+    dict_hobbies = {}
+    data = data.replace("\n", " ").split(" ")
+
+    for i in data:
+        i = i.split(":")
+        if i[1] not in dict_hobbies:
+            dict_hobbies[i[1]] = []
+        if i[0] not in dict_hobbies[i[1]]:
+            dict_hobbies[i[1]].append(i[0])
+
+    dict_hobbies = sort_dictionary(dict_hobbies)
+
+    return dict_hobbies
 
 
 def find_people_with_most_hobbies(data: str) -> list:
@@ -59,7 +76,22 @@ def find_people_with_most_hobbies(data: str) -> list:
     :param data: given string from database
     :return: list of people with most hobbies. Sorted alphabetically.
     """
-    pass
+    data = create_dictionary(data)
+    pupils_hobbies_count = {}
+    list_of_people_with_most_hobbies = []
+    max_value = 0
+
+    for k, v in data.items():
+        v = len(v)
+        pupils_hobbies_count[k] = v
+        if v > max_value:
+            max_value = v
+
+    for k, v in pupils_hobbies_count.items():
+        if v == max_value:
+            list_of_people_with_most_hobbies.append(k)
+
+    return sorted(list_of_people_with_most_hobbies)
 
 
 def find_people_with_least_hobbies(data: str) -> list:
@@ -69,7 +101,22 @@ def find_people_with_least_hobbies(data: str) -> list:
     :param data: given string from database
     :return: list of people with least hobbies. Sorted alphabetically.
     """
-    pass
+    data = create_dictionary(data)
+    pupils_hobbies_count = {}
+    list_of_people_with_most_hobbies = []
+    max_value = 999
+
+    for k, v in data.items():
+        v = len(v)
+        pupils_hobbies_count[k] = v
+        if v < max_value:
+            max_value = v
+
+    for k, v in pupils_hobbies_count.items():
+        if v == max_value:
+            list_of_people_with_most_hobbies.append(k)
+
+    return sorted(list_of_people_with_most_hobbies)
 
 
 def find_most_popular_hobbies(data: str) -> list:
@@ -79,7 +126,22 @@ def find_most_popular_hobbies(data: str) -> list:
     :param data: given string from database
     :return: list of most popular hobbies. Sorted alphabetically.
     """
-    pass
+    data = create_dictionary_with_hobbies(data)
+    hobbies_popularity = {}
+    most_popular_hobbies = []
+    max_value = 0
+
+    for k, v in data.items():
+        v = len(v)
+        hobbies_popularity[k] = v
+        if v > max_value:
+            max_value = v
+
+    for k, v in hobbies_popularity.items():
+        if v == max_value:
+            most_popular_hobbies.append(k)
+
+    return sorted(most_popular_hobbies)
 
 
 def find_least_popular_hobbies(data: str) -> list:
@@ -89,7 +151,22 @@ def find_least_popular_hobbies(data: str) -> list:
     :param data: given string from database
     :return: list of least popular hobbies. Sorted alphabetically.
     """
-    pass
+    data = create_dictionary_with_hobbies(data)
+    hobbies_popularity = {}
+    most_popular_hobbies = []
+    max_value = 99999
+
+    for k, v in data.items():
+        v = len(v)
+        hobbies_popularity[k] = v
+        if v < max_value:
+            max_value = v
+
+    for k, v in hobbies_popularity.items():
+        if v == max_value:
+            most_popular_hobbies.append(k)
+
+    return sorted(most_popular_hobbies)
 
 
 def sort_names_and_hobbies(data: str) -> tuple:
@@ -113,6 +190,8 @@ def sort_names_and_hobbies(data: str) -> tuple:
 if __name__ == '__main__':
     sample_data = """Jack:crafting\nPeter:hiking\nWendy:gaming\nMonica:tennis\nChris:origami\nSophie:sport\nMonica:design\nCarmen:sport\nChris:sport\nMonica:skateboarding\nCarmen:cooking\nWendy:photography\nMonica:tennis\nCooper:yoga\nWendy:sport\nCooper:movies\nMonica:theatre\nCooper:yoga\nChris:gaming\nMolly:fishing\nJack:skateboarding\nWendy:fishing\nJack:drawing\nMonica:baking\nSophie:baking\nAlfred:driving\nAlfred:shopping\nAlfred:crafting\nJack:drawing\nCarmen:shopping\nCarmen:driving\nPeter:drawing\nCarmen:shopping\nWendy:fitness\nAlfred:travel\nJack:origami\nSophie:design\nJack:pets\nCarmen:dance\nAlfred:baking\nSophie:sport\nPeter:gaming\nJack:skateboarding\nCooper:football\nAlfred:sport\nCooper:fitness\nChris:yoga\nWendy:football\nMolly:design\nJack:hiking\nMonica:pets\nCarmen:photography\nJack:baking\nPeter:driving\nChris:driving\nCarmen:driving\nPeter:theatre\nMolly:hiking\nWendy:puzzles\nJack:crafting\nPeter:photography\nCarmen:theatre\nSophie:crafting\nCarmen:cooking\nAlfred:gaming\nPeter:theatre\nCooper:hiking\nChris:football\nChris:pets\nJack:football\nMonica:skateboarding\nChris:driving\nCarmen:pets\nCooper:gaming\nChris:hiking\nJack:cooking\nPeter:fishing\nJack:gaming\nPeter:origami\nCarmen:movies\nSophie:driving\nJack:sport\nCarmen:theatre\nWendy:shopping\nCarmen:pets\nWendy:gaming\nSophie:football\nWendy:theatre\nCarmen:football\nMolly:theatre\nPeter:theatre\nMonica:flowers\nMolly:skateboarding\nPeter:driving\nSophie:travel\nMonica:photography\nCooper:cooking\nJack:fitness\nPeter:cooking\nChris:gaming"""
     dic = create_dictionary(sample_data)
+    print(sort_dictionary(dic))
+    print(create_dictionary_with_hobbies(sample_data))
     print("shopping" in dic["Wendy"])  # -> True
     print("fitness" in dic["Sophie"])  # -> False
     print("gaming" in dic["Peter"])  # -> True
