15f515594e4121e32e97e025cda428c934f5b831
rapidi <rapidi@ttu.ee>
2021-10-01 00:44:20 +0300

    :(

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -18,14 +18,22 @@ def generate_combined_list(inputs: list) -> list:
     For each element of 'inputs',
     it must be true that the returned list contains at least 'amount' of elements of type 'data_type'.
     """
-    # data_types = {"int": 0, "float": 0.1, "string": "0", "list": [], "tuple": (), "dict": {}, "set": set()}
-    # result = []
-    # for input in data_types.items():
-    #     result += generate_list(input[0], input[1])
-    #
-    # return result
-    pass
+    data_types = {"int": 0, "float": 0.1, "string": "0", "list": [], "tuple": (), "dict": {}, "set": set()}
+    data_typees = {}
+    result = []
 
+    for i in inputs:
+        if i[1] not in data_typees.keys():
+            data_typees[i[1]] = i[0]
+        if i[1] in data_typees.keys() and data_typees[i[1]] < i[0]:
+            data_typees[i[1]] = i[0]
+
+    for k, v in data_typees.items():
+        if k in data_types.keys():
+            for i in range(v):
+                result.append(data_types[k] * v)
+
+    return result
 
 def generate_combined_list_unique(inputs: list) -> list:
     """
@@ -57,12 +65,12 @@ if __name__ == '__main__':
     # The given outputs are only some of possible outputs,
     # for example for (3, 'string') in the first part an output of ["kass", "koer", "kana"] would also work.
 
-    # Part 1
-    print(generate_list(2, 'set'))  # [set(), set()]
-    print(generate_list(3, 'string'))  # ["a", "kass", "a"]
-    print(generate_list(1, 'list'))  # [[]]
-    print(generate_list(5, 'int'))  # [1, 2, 3, 3, 3]
-    print(generate_list(5, 'float'))
+    # # Part 1
+    # print(generate_list(2, 'set'))  # [set(), set()]
+    # print(generate_list(3, 'string'))  # ["a", "kass", "a"]
+    # print(generate_list(1, 'list'))  # [[]]
+    # print(generate_list(5, 'int'))  # [1, 2, 3, 3, 3]
+    # print(generate_list(5, 'float'))
 
     # Part 2
     print(generate_combined_list([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
@@ -71,14 +79,14 @@ if __name__ == '__main__':
     print(generate_combined_list([(2, 'list'), (3, 'string')]))  # ["a", [], "a", [], "a"]
     print(generate_combined_list([(2, 'float'), (3, 'dict')]))  # [{}, {}, {}, 3.14, 3.15]
     print()
-
-    # Part 3
-    print(generate_combined_list_unique([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
-    print(generate_combined_list_unique([(2, 'int'), (2, 'float'), (1, 'int')]))  # [43, 93, 4.3, 2.1]
-    print()
-
-    # Part 4
-    print(generate_combined_list_unique_advanced([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
-    print(generate_combined_list_unique_advanced([(2, 'list'), (3, 'string')]))  # ["a", [2], "asd", [], "abc"]
-    print(generate_combined_list_unique_advanced([(2, 'float'), (3, 'dict')]))
-    print()
+    #
+    # # Part 3
+    # print(generate_combined_list_unique([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
+    # print(generate_combined_list_unique([(2, 'int'), (2, 'float'), (1, 'int')]))  # [43, 93, 4.3, 2.1]
+    # print()
+    #
+    # # Part 4
+    # print(generate_combined_list_unique_advanced([(3, 'int'), (5, 'int')]))  # [1, 2, 3, 4, 5]
+    # print(generate_combined_list_unique_advanced([(2, 'list'), (3, 'string')]))  # ["a", [2], "asd", [], "abc"]
+    # print(generate_combined_list_unique_advanced([(2, 'float'), (3, 'dict')]))
+    # print()
