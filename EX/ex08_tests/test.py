45c456c0b7ab9164a43ab73eb9717ee897bed577
rapidi <rapidi@ttu.ee>
2021-10-25 19:12:31 +0300

    k?

--- EX/ex08_tests/test.py
+++ EX/ex08_tests/test.py
@@ -2,59 +2,16 @@ import pytest
 import solution
 
 
-def test_part1_int_correct_len():
-    input_amount = 5
+def test_data_types():
+    data_types = {"int": int, "float": float, "string": str, "list": list, "tuple": tuple, "dict": dict, "set": set}
+    for key, value in data_types.items():
+        res = solution.generate_list(5, key)
+        for element in res:
+            assert isinstance(element, value)
+
+
+def test_part1_input_zero():
+    input_amount = 0
     res = solution.generate_list(input_amount, "int")
-    expected_len = 5
-    assert len(res) == expected_len
-
-
-def test_part1_int_correct_content():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "int")
-    expected_len = 5
-    assert len(res) == expected_len
-    for element in res:
-        assert isinstance(element, int)
-
-
-def test_part1_float_correct_len():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "float")
-    expected_len = 5
-    assert len(res) == expected_len
-
-
-def test_part1_string_correct_len():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "string")
-    expected_len = 5
-    assert len(res) == expected_len
-
-
-def test_part1_list_correct_len():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "list")
-    expected_len = 5
-    assert len(res) == expected_len
-
-
-def test_part1_dict_correct_len():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "dict")
-    expected_len = 5
-    assert len(res) == expected_len
-
-
-def test_part1_tuple_correct_len():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "tuple")
-    expected_len = 5
-    assert len(res) == expected_len
-
-
-def test_part1_set_correct_len():
-    input_amount = 5
-    res = solution.generate_list(input_amount, "set")
-    expected_len = 5
+    expected_len = 0
     assert len(res) == expected_len
