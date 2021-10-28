1d322147fac01fa8a07b0d454de45bca2e850ebd
rapidi <rapidi@ttu.ee>
2021-10-28 12:36:55 +0300

    >(/.

---
+++ EX/ex08_solution_and_tests/solution.py
@@ -0,0 +1,39 @@
+def students_study(time: int, coffee_needed: bool) -> bool:
+    """
+    Return True if students study in given circumstances.
+
+    (19, False) -> True
+    (1, True) -> False.
+    """
+    if time in range(18, 25) or range(1, 5):
+        coffee_needed = False
+    if time in range(5, 18):
+        coffee_needed = True
+
+    return coffee_needed
+
+
+def lottery(a: int, b: int, c: int) -> int:
+    """
+    Return Lottery victory result 10, 5, 1, or 0 according to input values.
+
+    (5, 5, 5) -> 10
+    (2, 2, 1) -> 0
+    (2, 3, 1) -> 1
+    """
+    pass
+
+
+def fruit_order(small_baskets: int, big_baskets: int, ordered_amount: int) -> int:
+    """
+    Return number of small fruit baskets if it's possible to finish the order, otherwise return -1.
+
+    (4, 1, 9) -> 4
+    (3, 1, 10) -> -1
+    """
+    pass
+
+
+if __name__ == '__main__':
+    for i in range(2, 6):
+        print(i)
