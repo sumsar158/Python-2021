c7f7f37b89b8275f7cc982120dead4c0e4924422
rapidi <17.oktoober>
2021-12-15 17:33:51 +0200

    hertqa

--- EX/ex11_order/order.py
+++ EX/ex11_order/order.py
@@ -1,5 +1,6 @@
 """Order system."""
 
+
 class OrderItem:
     """Order Item requested by a customer."""
 
@@ -101,7 +102,7 @@ class OrderAggregator:
         :param item: Item to add.
         :return: None
         """
-        pass
+        self.order_items.append(OrderItem)
 
     def aggregate_order(self, customer: str, max_items_quantity: int, max_volume: int):
         """
@@ -155,37 +156,37 @@ if __name__ == '__main__':
 
     print(order_item3.total_volume)  # 60000
 
-    # print("Order Aggregator")
-    # oa = OrderAggregator()
-    # oa.add_item(order_item1)
-    # oa.add_item(order_item2)
-    # oa.add_item(order_item3)
-    # oa.add_item(order_item4)
-    # oa.add_item(order_item5)
-    # oa.add_item(order_item6)
-    # print(f'Added {len(oa.order_items)}(6 is correct) order items')
-    #
-    # order1 = oa.aggregate_order("Apple", 350, 3000)
-    # order1.destination = "Tallinn"
-    # print(f'order1 has {len(order1.order_items)}(2 is correct) order items')
-    #
-    # order2 = oa.aggregate_order("Mööbel 24", 325, 64100)
-    # order2.destination = "Tallinn"
-    # print(f'order2 has {len(order2.order_items)}(2 is correct) order items')
-    #
-    # print(f'after orders creation, aggregator has only {len(oa.order_items)}(2 is correct) order items left.')
-    #
-    # print("Container Aggregator")
-    # ca = ContainerAggregator(70000)
-    # too_big_order = Order([OrderItem("Apple", "Apple Car", 10000, 300)])
-    # too_big_order.destination = "Somewhere"
-    # containers = ca.prepare_containers((order1, order2, too_big_order))
-    # print(f'prepare_containers produced containers to {len(containers)}(1 is correct) different destination(s)')
-    #
-    # try:
-    #     containers_to_tallinn = containers['Tallinn']
-    #     print(f'volume of the container to tallinn is {containers_to_tallinn[0].volume}(70000 is correct) cm^3')
-    #     print(f'container to tallinn has {len(containers_to_tallinn[0].orders)}(2 is correct) orders')
-    # except KeyError:
-    #     print('Container to Tallinn not found!')
-    # print(f'{len(ca.not_used_orders)}(1 is correct) cannot be added to containers')
+    print("Order Aggregator")
+    oa = OrderAggregator()
+    oa.add_item(order_item1)
+    oa.add_item(order_item2)
+    oa.add_item(order_item3)
+    oa.add_item(order_item4)
+    oa.add_item(order_item5)
+    oa.add_item(order_item6)
+    print(f'Added {len(oa.order_items)}(6 is correct) order items')
+
+    order1 = oa.aggregate_order("Apple", 350, 3000)
+    order1.destination = "Tallinn"
+    print(f'order1 has {len(order1.order_items)}(2 is correct) order items')
+
+    order2 = oa.aggregate_order("Mööbel 24", 325, 64100)
+    order2.destination = "Tallinn"
+    print(f'order2 has {len(order2.order_items)}(2 is correct) order items')
+
+    print(f'after orders creation, aggregator has only {len(oa.order_items)}(2 is correct) order items left.')
+
+    print("Container Aggregator")
+    ca = ContainerAggregator(70000)
+    too_big_order = Order([OrderItem("Apple", "Apple Car", 10000, 300)])
+    too_big_order.destination = "Somewhere"
+    containers = ca.prepare_containers((order1, order2, too_big_order))
+    print(f'prepare_containers produced containers to {len(containers)}(1 is correct) different destination(s)')
+
+    try:
+        containers_to_tallinn = containers['Tallinn']
+        print(f'volume of the container to tallinn is {containers_to_tallinn[0].volume}(70000 is correct) cm^3')
+        print(f'container to tallinn has {len(containers_to_tallinn[0].orders)}(2 is correct) orders')
+    except KeyError:
+        print('Container to Tallinn not found!')
+    print(f'{len(ca.not_used_orders)}(1 is correct) cannot be added to containers')
