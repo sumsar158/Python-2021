3b371afca8acbc084a0df899609dab0f817331de
rapidi <17.oktoober>
2022-01-14 22:33:34 +0200

    follow th eline

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -87,6 +87,7 @@ def follow_the_line(robot: FollowerBot):
     robot.done()
 
 
+
 def the_true_follower(robot: FollowerBot):
     """
     Create a FollowerBot that will follow the black line on the track and make it ignore all possible distractions.
@@ -124,29 +125,29 @@ def the_true_follower(robot: FollowerBot):
             robot.set_right_wheel_speed(-15)
             robot.sleep(0.001)
 
+
         elif robot.get_position() == (236, 228):
             for i in range(70):
                 robot.set_wheels_speed(100)
                 robot.sleep(0.01)
-            continue
-        elif 0 < sum(robot.get_line_sensors()) < 1024:
-            robot.set_left_wheel_speed(-50)
-            robot.set_right_wheel_speed(50)
-            robot.sleep(0.01)
-            continue
+
 
         else:
             robot.set_wheels_speed(65)
             robot.sleep(0.01)
 
-    print("------Line Lost------")
-    print(robot.get_line_sensors())
-    print(robot.get_position())
-    print("------Line Lost------")
+        if robot.get_position() == (126, 136):
+            robot.set_left_wheel_speed(-100)
+            robot.set_right_wheel_speed(100)
+            robot.sleep(0.3)
 
-    robot.done()
+    for i in range(8):
+        robot.set_wheels_speed(100)
+        robot.sleep(0.1)
+    follow_the_line(robot)
+
 
 
 if __name__ == '__main__':
-    robot = FollowerBot(track_image='track.png', start_x=265, start_y=310)
+    robot = FollowerBot(track_image='track.png', start_x=265, start_y=310, timeout=180)
     the_true_follower(robot)
